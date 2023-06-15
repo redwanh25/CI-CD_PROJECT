dc433f4141fed6f10b92bddbfce47f085ddd75f6
Redwan Hossain <redwanh25@gmail.com>
2023-06-16 02:50:02 +0600

    fafsa

--- CI-CD_PROJECT/Controllers/HomeController.cs
+++ CI-CD_PROJECT/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CI_CD_PROJECT.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Name = "amr shonar bangla";
+            ViewBag.Name = "amr shona";
             return View();
         }
 

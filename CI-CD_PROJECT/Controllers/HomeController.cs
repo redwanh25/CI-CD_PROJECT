72ba41eae1ca60540da4d17838ad479813bc1575
redwan hossain <redwan.hossain@brainstation-23.com>
2023-06-17 15:00:15 +0600

    fsfs

--- CI-CD_PROJECT/Controllers/HomeController.cs
+++ CI-CD_PROJECT/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CI_CD_PROJECT.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Name = "Redwan Babu shona";
+            ViewBag.Name = "Redwan";
             return View();
         }
 

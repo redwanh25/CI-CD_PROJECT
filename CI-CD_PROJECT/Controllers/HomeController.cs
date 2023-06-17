287f174fd26fec71d62067bfb459e26781f96465
Redwan Hossain <redwanh25@gmail.com>
2023-06-17 16:21:46 +0600

    gasds

--- CI-CD_PROJECT/Controllers/HomeController.cs
+++ CI-CD_PROJECT/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CI_CD_PROJECT.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Name = "Redwan Hossain 1";
+            ViewBag.Name = "Redwan Hossain";
             return View();
         }
 

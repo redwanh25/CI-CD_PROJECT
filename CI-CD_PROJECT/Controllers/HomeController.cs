a5cb588a1f97964ac12eac797cf7fae01a5882b6
Redwan Hossain <redwan.hossain@metlife.com.bd>
2023-06-18 11:41:48 +0600

    fasfs

--- CI-CD_PROJECT/Controllers/HomeController.cs
+++ CI-CD_PROJECT/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CI_CD_PROJECT.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Name = "Redwan Hossain";
+            ViewBag.Name = "Redwan 456";
             return View();
         }
 

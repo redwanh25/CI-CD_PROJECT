28c6707aea052f452dd548d3b8cc2624e97f35a5
redwan hossain <redwan.hossain@brainstation-23.com>
2023-06-18 12:14:54 +0600

    lkjlfs

--- CI-CD_PROJECT/Controllers/HomeController.cs
+++ CI-CD_PROJECT/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CI_CD_PROJECT.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Name = "Redwan 456";
+            ViewBag.Name = "Redwan";
             return View();
         }
 
